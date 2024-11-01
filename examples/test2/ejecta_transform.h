fb8b678af203dee0518d1a451e8b32567bf89353
Linfeng Li <linfel@umich.edu>
2024-11-01 11:36:47 -0400

    remove folder: test2 test3

--- examples/test2/ejecta_transform.h
+++
@@ -1,9 +0,0 @@
-typedef struct {
-    double ID;
-    double x, y, z;
-    double vx, vy, vz;
-    double mass, density;
-} ReadParticle;
-
-// Function declarations
-void transform(ReadParticle *p, double x_translation);
