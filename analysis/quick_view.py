318c578d0406076e0033737b31613e15d508b983
linfel <linfel@umich.edu>
2025-12-04 11:27:11 -0500

    quick_view.py: add function to print out time metadata of particles.txt

--- analysis/quick_view.py
+++ analysis/quick_view.py
@@ -5,7 +5,7 @@ Quick plot top view, HST view and side view
 import os, glob
 import pickle
 import numpy as np
-from ReadParticle import read_specific_frame
+from ReadParticle import read_specific_frame, read_meta
 from frame_renderer import init_worker, render_frame_topview, render_frame_HSTview, render_frame_sideview
 
 #------------------------------------------------------------------------------
@@ -35,6 +35,19 @@ def quick_plot_particles():
 		data_path = os.path.join(DATA_ROOTDIR, f"run_{run_idx:03d}", "particles.txt")
 		output_dir = os.path.join(DATA_ROOTDIR, f"run_{run_idx:03d}", "quickview")
 		quickview_3(data_path, output_dir, PLOT_DAYS, AXIS_LIM)
+
+def check_metadata():
+	"""
+	print out time in days of data particles.txt
+	"""
+	DATA_ROOTDIR = "/home/linfel/linfel_turbo/rebound_exp/data_high_shortterm_run"
+	RUN_NUMBERS = range(34, 51) # Define the run numbers you want to process
+	
+	for run_idx in RUN_NUMBERS:
+		print(f"processing run_{run_idx:03d}")
+		data_path = os.path.join(DATA_ROOTDIR, f"run_{run_idx:03d}", "particles.txt")
+		Np, time = read_meta(data_path)
+		print(f"time: {time/86400.} day\n")
 
 #------------------------------------------------------------------------------
 def check_pkl_structure(folder_path):
@@ -78,4 +91,5 @@ def check_pkl_structure(folder_path):
 #------------------------------------------------------------------------------
 if __name__ == "__main__":
 	#quick_plot_particles()
-	check_pkl_structure("/home/linfel/linfel_scratch/rebound_exp/data_high_longterm_run_034-050/snapshot_data")
+	#check_pkl_structure("/home/linfel/linfel_scratch/rebound_exp/data_high_longterm_run_034-050/snapshot_data")
+	check_metadata()
